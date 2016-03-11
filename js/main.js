0f4070b52d5a5802e9fb3200b25fde431677bd0b
KimothyPepper <kim-pep@hotmail.com>
2016-03-11 08:50:44 -0500

    Added Content to js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var $h1 = $('h1');
+var $ul = $('ul');
+var $li = $('li');
+var $h2 = $('h2');
+
+$h1.html('Below ground veggies');
+$h2.html('Above ground veggies');
+
+$('h1').append('<h1>Below ground veggies</h1>');
+$('h2').append('<h2>Above ground veggies</h2>');
